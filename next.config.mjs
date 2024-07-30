60b5471930b7e00651f0156d0fc82e153fc88a7a
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2024-07-30 13:00:11 -0400

    change project name in sentry config

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ export default withSentryConfig(nextConfig, {
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 org: "eagle-team",
-project: "javascript-nextjs",
+project: "carepulse",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
